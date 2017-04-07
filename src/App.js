dab6620e3c0fcb94f3efb4e0b792c940f8959167
Nic <nwentling5@gmail.com>
2017-04-07 14:15:50 -0500

    Added the skeleton of the UI system and Taks paneling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import {Appbar, Container} from 'muicss/react';
+import Tasks from './Components/Tasks';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-
+				<Appbar>
+					<Container>
+						<table width="100%">
+							<tbody>
+							<tr>
+								<td className="mui--appbar-height"><h3>React Tasks</h3></td>
+							</tr>
+							</tbody>
+						</table>
+					</Container>
+				</Appbar>
+				<br />
+				<Container>
+					<Tasks />
+				</Container>
 			</div>
 		);
 	}
---
+++ src/Components/Tasks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Panel} from 'muicss/react';
+
+
+class Tasks extends Component {
+	render() {
+		return (
+			<Panel>
+				Tasks
+			</Panel>
+		);
+	}
+}
+
+export default Tasks;
